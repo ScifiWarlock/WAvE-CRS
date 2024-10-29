27cd7f62000a5b3d6e378f8fc97510f28b9b7c8c
ScifiWarlock <95051950+ScifiWarlock@users.noreply.github.com>
2024-10-29 15:13:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,11 @@ from subprocess import run
 
 stdf = pd.read_excel("df.xlsx")
 
+stdf = stdf.iloc[:, [-1]]
+
 st.title("WAvE CRS")
 st.write(
-    "Welcome to the WAvE Customer Reporting Service. Below is a bar chart of daily fatigue occurences."
+    "Welcome to the WAvE Customer Reporting Service. Below is a table of daily fatigue occurences."
 )
 st.write("We will be adding more data visualizations in the next few months. Stay tuned!")
 st.table(stdf, columns='Logs')
