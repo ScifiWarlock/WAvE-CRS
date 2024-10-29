d0f98e1f631b72e24144bf1c2b459c05cc8b9e87
ScifiWarlock <95051950+ScifiWarlock@users.noreply.github.com>
2024-10-29 11:15:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,4 @@ st.write(
     "Welcome to the WAvE Customer Reporting Service. Below is a bar chart of daily fatigue occurences."
 )
 st.write("We will be adding more data visualizations in the next few months. Stay tuned!")
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.bar_chart(chart_data)
+st.bar_chart([2, 3, 2, 3], "day", "logs")
