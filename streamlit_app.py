7c80983123341d05ca9439129e01cc950feecf03
ScifiWarlock <95051950+ScifiWarlock@users.noreply.github.com>
2024-10-29 12:31:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ import pandas as pd
 import numpy as np
 
 #import df from wavetest
-from mywavetest import get_df
+#from mywavetest import get_df
 
-stdf = get_df()
+#stdf = get_df()
+
+log_array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+stdf = pd.DataFrame(log_array, columns=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 
 st.title("WAvE CRS")
 st.write(
