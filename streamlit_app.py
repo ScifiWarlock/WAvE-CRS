cc2a85bce3cefa7925fc28b89c49f4ec83f30bcd
ScifiWarlock <95051950+ScifiWarlock@users.noreply.github.com>
2024-10-29 12:07:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#import df from wavetest
+from mywavetest import get_df
+
+stdf = get_df()
+
 st.title("WAvE CRS")
 st.write(
     "Welcome to the WAvE Customer Reporting Service. Below is a bar chart of daily fatigue occurences."
 )
 st.write("We will be adding more data visualizations in the next few months. Stay tuned!")
-st.bar_chart([2, 3, 2, 3], "day", "logs")
+st.bar_chart(stdf)
